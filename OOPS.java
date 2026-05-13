af43c13b919b1916fffc472b62c1e0c0a88c8708
Ritika Singh <ritikasinghharshjeetsingh@gmail.com>
2026-05-13 23:21:33 +0530

    Restore all files

---
+++ OOPS.java
@@ -0,0 +1,55 @@
+public class OOPS { 
+    public static void main(String args[]) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.print(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Ritika";
+        myAcc.setPassword("@singh");
+    }
+}
+
+class Pen {
+    private String color;
+    int tip;
+    
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int Tip) {
+        this.tip = Tip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calcPercentage(int phy, int chem, int math) {
+        percentage = (phy + chem + math) / 3;
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd) {
+        password = pwd;
+    }
+}
