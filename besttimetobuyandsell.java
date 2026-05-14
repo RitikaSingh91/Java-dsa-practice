7cdb99ae876d1a511f4a3f104e6e1a17f01232a7
Ritika Singh <ritikasinghharshjeetsingh@gmail.com>
2026-05-14 22:35:54 +0530

    Add BestTimeToBuyAndSell solution

---
+++ besttimetobuyandsell.java
@@ -0,0 +1,28 @@
+public class BestTimeToBuyAndSell {
+
+    public static int maxProfit(int[] prices) {
+
+        int buy = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > maxProfit) {
+                maxProfit = prices[i] - buy;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+
+        System.out.println(result);
+    }
+}
